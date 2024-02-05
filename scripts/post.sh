e25a673a591812640af748e9f630b31ebf660f7a
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2024-02-05 21:35:50 +0100

    scripts/post.sh: minor typo

--- scripts/post.sh
+++ scripts/post.sh
@@ -6,7 +6,7 @@ DATE=$(date +"%Y-%m-%d")
 echo "date: ${DATE}"
 read -p "title: " TITLE
 DEFAULT_SLUG=$(npx slugify-cli "${TITLE}")
-read -p "slug [$DEFAULT_SLUG}]: " SLUG
+read -p "slug [${DEFAULT_SLUG}]: " SLUG
 SLUG=${SLUG:-${DEFAULT_SLUG}}
 POST_DIR="${BASH_SOURCE%/*}"/../blog/$(date +"%Y")/$(date +"%m")/$SLUG
 POST=$POST_DIR/index.md
