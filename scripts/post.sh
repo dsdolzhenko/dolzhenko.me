70bcfedbde2794cab7302aa43405d051b989c4aa
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2024-07-03 14:19:52 +0200

    scripts: fix shellcheck warnings in post.sh

--- scripts/post.sh
+++ scripts/post.sh
@@ -4,16 +4,16 @@ set -e
 
 DATE=$(date -Iminutes -u)
 echo "date: ${DATE}"
-read -e -p "title: " TITLE
+read -r -e -p "title: " TITLE
 DEFAULT_SLUG=$(npx slugify-cli "${TITLE}")
-read -e -p "slug [${DEFAULT_SLUG}]: " SLUG
+read -r -e -p "slug [${DEFAULT_SLUG}]: " SLUG
 SLUG=${SLUG:-${DEFAULT_SLUG}}
 POST_DIR="${BASH_SOURCE%/*}"/../src/blog/$(date +"%Y")/$(date +"%m")/$SLUG
 POST=$POST_DIR/index.md
 
 if [[ ! -f $POST ]]; then
-    mkdir -p $POST_DIR
-    cat > $POST << EOL
+    mkdir -p "${POST_DIR}"
+    cat > "${POST}" << EOL
 ---
 date:  ${DATE}
 title: ${TITLE}
@@ -21,4 +21,4 @@ title: ${TITLE}
 EOL
 fi
 
-"${EDITOR:-vi}" $POST
+"${EDITOR:-vi}" "${POST}"
