4a373f50277779c3c2f99639d9781896795e88a1
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2025-01-05 11:02:33 +0100

    Change hostname for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 cd "${BASH_SOURCE%/*}"/..
 
-rsync -razP --delete dist/ dolzhenko@dolzhenko.dev:/var/www/dolzhenko.me
+rsync -razP --delete dist/ dolzhenko@dolzhenko.me:/var/www/dolzhenko.me
